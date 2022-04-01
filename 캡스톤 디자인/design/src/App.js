f0dbd1ec91582842c2a00c00785b4883864e2ffe
hanav52 <wnsrl8329@gmail.com>
2022-04-01 22:41:52 +0900

    2022-04-01 캡스톤 디자인

---
+++ 캡스톤 디자인/design/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+//import './App.css';
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <h1>welcome</h1>
+      <Button text={"continue"}/>
+    </div>
+  );
+}
+
+export default App;
