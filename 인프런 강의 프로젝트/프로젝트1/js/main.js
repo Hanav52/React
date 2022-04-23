f6128adde56401603a4bc9cad1311ab9688ba63d
hanav52 <wnsrl8329@gmail.com>
2022-04-23 20:06:42 +0900

    2022-04-23
    arrow function 사용

--- 인프런 강의 프로젝트/프로젝트1/js/main.js
+++ 인프런 강의 프로젝트/프로젝트1/js/main.js
@@ -1,4 +1,12 @@
 const backToTop = document.getElementById('backtotop');
 
+const checkScroll = () => {
+
+}
+
+function moveBackToTop() {
+    
+}
+
 window.addEventListener('scroll', checkScroll);
 backToTop.addEventListener('click', moveBackToTop);
