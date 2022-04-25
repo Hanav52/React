0f08444ec1e1de5fd2f301cf83568f076f60a3ea
hanav52 <wnsrl8329@gmail.com>
2022-04-25 23:48:49 +0900

    2022-04-25
    설명

--- 캡스톤 디자인/design/src/App copy.js
+++ 캡스톤 디자인/design/src/App copy.js
@@ -37,4 +37,24 @@ function App() {
 console.log("CALL THE API")
 },[]); 써도 됨
 state를 변경할때는 함수가 다시 시작된다.*/
+
+/*
+useEffect 글자를 타이핑할 때마다 API를 새로 호출하지 않고 한번만 호출해준다.
+
+search keyword에 변화가 있을 때 만! marvel영화를 검색하고 싶을 때
+즉, 특정한 코드만 변화했을 때 원하는 코드들을 실행할 수 있는 방법
+-> useEffect(() => {
+console.log("SEARCH FOR", keyword);
+}, []);
+이렇게 실행하면 1번만 됨
+그리고 []자리에 keyword 써줌
+-> keyword가 변화할 때 코드를 실행할 거라고 react.js에게 알려주는 것.
+이것이 바로 빈 array를 써주었을 때 코드가 단 한번만 실행되는 이유임
+react가 지켜볼 게 아무것도 없으니 처음 한번만 실행되는 것
+
+useEffect(() => {
+console.log("I run when 'keyword & counter' changes.")
+}, [keyword, counter]);
+-> 2개도 가능
+*/
 export default App;
