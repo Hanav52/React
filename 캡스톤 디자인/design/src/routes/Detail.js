f9e80c243d6707cc0d51827e841f913e381a9acb
hanav52 <wnsrl8329@gmail.com>
2022-06-18 20:46:03 +0900

    영화 사이트

--- 캡스톤 디자인/design/src/routes/Detail.js
+++ 캡스톤 디자인/design/src/routes/Detail.js
@@ -1,16 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Movie from "../components/Movie";
+
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setMovies(json.data.moives);
+    setLoading(false);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      {loading ? (<div>Loading...</div>) : (<div>true
+        {movies.id}
+      </div>)}
+    </div>
+  );
 }
 export default Detail;
